a0073ca41bebe91aa7b97326de3a7aa85a4990a2
damirkap89 <damirkap89@gmail.com>
2021-08-31 15:02:12 +0600

    adding check for valid len

--- main.go
+++ main.go
@@ -8,15 +8,23 @@ import (
 )
 
 func main() {
-
 	if len(os.Args) != 2 {
 		fmt.Println("No arguments")
 		return
 	}
 	argMain := os.Args[1]
-
 	argsArr := strings.Split(argMain, "\\n")
-	file, _ := ioutil.ReadFile("fonts/standard.txt")
+	file, err := ioutil.ReadFile("fonts/standard.txt")
+	lv := len(file)
+	if lv != 6623 {
+		fmt.Println("File is corrupted")
+		return
+	}
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 	arr := []string{}
 	for _, el := range strings.Split(string(file), string('\n')) {
 		arr = append(arr, el)
@@ -29,7 +37,7 @@ func main() {
 		for x := 0; x < 8; x++ {
 			for _, el := range arg {
 				if el < 32 || el > 126 {
-					fmt.Println("ERROR")
+					fmt.Println("Error: non valid ASCII symbol")
 					return
 				}
 				n := (el-32)*9 + 1
