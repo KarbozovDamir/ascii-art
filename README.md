028c9618bd662e92351c946abd74a781ae1fae40
damirkap89 <damirkap89@gmail.com>
2021-08-31 16:03:55 +0600

    added readme and const

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ascii-art project
+>Ascii-art is a program that recieves a string as an argument, 
+>and outputs the string in big letters using ASCII
--- main.go
+++ main.go
@@ -7,28 +7,37 @@ import (
 	"strings"
 )
 
+const (
+	FILE_LEN = 6623
+)
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("No arguments")
 		return
 	}
 	argMain := os.Args[1]
-	argsArr := strings.Split(argMain, "\\n")
-	file, err := ioutil.ReadFile("fonts/standard.txt")
-	lv := len(file)
-	if lv != 6623 {
-		fmt.Println("File is corrupted")
+	if !isValid(argMain) {
+		fmt.Println("Non-valid characters")
 		return
 	}
-
+	argsArr := strings.Split(strings.ReplaceAll(argMain, "\\n", "\n"), "\n")
+	file, err := ioutil.ReadFile("fonts/standard.txt")
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
+
+	if len(file) != FILE_LEN {
+		fmt.Println("File is corrupted")
+		return
+	}
+
 	arr := []string{}
 	for _, el := range strings.Split(string(file), string('\n')) {
 		arr = append(arr, el)
 	}
+
 	for _, arg := range argsArr {
 		if arg == "" {
 			fmt.Println()
@@ -36,15 +45,21 @@ func main() {
 		}
 		for x := 0; x < 8; x++ {
 			for _, el := range arg {
-				if el < 32 || el > 126 {
-					fmt.Println("Error: non valid ASCII symbol")
-					return
-				}
 				n := (el-32)*9 + 1
 				fmt.Print(arr[int(n)+x])
 			}
 			fmt.Println()
 		}
+		fmt.Println()
 	}
 
+}
+
+func isValid(s string) bool {
+	for _, ch := range s {
+		if ch < ' ' && ch != 10 || ch > '~' {
+			return false
+		}
+	}
+	return true
 }
